1e8cf2517b21f0e98449719a30e8c98c489d69bc
Miller <guf302010@gmail.com>
2021-09-11 21:31:56 +0700

    Initial Commit

---
+++ ColorApp 2/ColorApp_2App.swift
@@ -0,0 +1,17 @@
+//
+//  ColorApp_2App.swift
+//  ColorApp 2
+//
+//  Created by Robert Miller on 11.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorApp_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
