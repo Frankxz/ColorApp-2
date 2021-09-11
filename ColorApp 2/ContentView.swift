1301581bf558911c7fd11f6bd7f5c5056fdb5864
Miller <guf302010@gmail.com>
2021-09-12 01:11:45 +0700

    Set up labels, sliders and colorView

--- ColorApp 2/ContentView.swift
+++ ColorApp 2/ContentView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var redValue: Double = 0
+    @State private var greenValue: Double = 0
+    @State private var blueValue: Double = 0
+    
+    @State private var viewColor = Color(.white)
+    @State private var value = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Rectangle().ignoresSafeArea()
+            VStack {
+                
+                ColorView(red: redValue, green: greenValue, blue: blueValue)
+                VStack {
+                SliderStack(dValue: $redValue, sValue: $value, color: .red)
+                SliderStack(dValue: $greenValue, sValue: $value, color: .green)
+                SliderStack(dValue: $blueValue, sValue: $value, color: .blue)
+                }.padding()
+                Spacer()
+                
+            }
+        }
     }
 }
 
@@ -18,4 +37,43 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct SliderStack: View {
+    @Binding  var dValue: Double
+    @Binding  var sValue: String
+    var color: Color
+    var body: some View {
+        HStack {
+            Text("\(lround(dValue))").foregroundColor(.white)
+                .frame(width: 40)
+            Slider(value: $dValue, in: 0...255, step: 1).shadow(color: color, radius: 5)
+                .accentColor(color)
+            TextField("", text: $sValue)
+                .multilineTextAlignment(.center)
+                .frame(width: 45)
+                .foregroundColor(.white)
+                .overlay(RoundedRectangle(cornerRadius: 4)
+                            .stroke(lineWidth: 2)
+                            .foregroundColor(color))
+        }
+      
+    }
+}
+
+struct ColorView: View {
+    var red: Double
+    var green: Double
+    var blue: Double
+    
+    var body: some View {
+        RoundedRectangle(cornerSize: CGSize(width: 10, height: 10))
+            .frame(width: 340, height: 160)
+            .foregroundColor(Color(red: red/255,
+                                  green: green/255,
+                                  blue: blue/255))
+
+            .padding()
+    }
+    var color = Color(UIColor(red: 1, green: 1, blue: 1, alpha: 1))
 }
