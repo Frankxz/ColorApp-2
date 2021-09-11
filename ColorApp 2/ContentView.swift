c9931218a07b3aaa526c8de9d3921bbec97eec9a
Miller <guf302010@gmail.com>
2021-09-12 02:49:24 +0700

    Set up textfield Set up alerts

--- ColorApp 2/ContentView.swift
+++ ColorApp 2/ContentView.swift
@@ -13,21 +13,22 @@ struct ContentView: View {
     @State private var greenValue: Double = 0
     @State private var blueValue: Double = 0
     
-    @State private var viewColor = Color(.white)
-    @State private var value = ""
+   
+    @State private var redValueTF = "0"
+    @State private var greenValueTF = "0"
+    @State private var blueValueTF = "0"
+    
     var body: some View {
         ZStack{
             Rectangle().ignoresSafeArea()
             VStack {
-                
                 ColorView(red: redValue, green: greenValue, blue: blueValue)
                 VStack {
-                SliderStack(dValue: $redValue, sValue: $value, color: .red)
-                SliderStack(dValue: $greenValue, sValue: $value, color: .green)
-                SliderStack(dValue: $blueValue, sValue: $value, color: .blue)
+                SliderStack(dValue: $redValue, sValue: $redValueTF, color: .red)
+                SliderStack(dValue: $greenValue, sValue: $greenValueTF, color: .green)
+                SliderStack(dValue: $blueValue, sValue: $blueValueTF, color: .blue)
                 }.padding()
                 Spacer()
-                
             }
         }
     }
@@ -40,24 +41,51 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 struct SliderStack: View {
+    @State private var showAlert = false
+    
     @Binding  var dValue: Double
     @Binding  var sValue: String
+    
     var color: Color
+
     var body: some View {
         HStack {
-            Text("\(lround(dValue))").foregroundColor(.white)
+            Text("\(lround(dValue))")
+                .foregroundColor(.white)
                 .frame(width: 40)
-            Slider(value: $dValue, in: 0...255, step: 1).shadow(color: color, radius: 5)
+
+            Slider(value: $dValue, in: 0...255, step: 1)
+                { _ in sValue = "\(lround(dValue))" }
+                .shadow(color: color, radius: 5)
                 .accentColor(color)
-            TextField("", text: $sValue)
+            
+            TextField("", text: $sValue, onCommit: checkValue)
+                .alert(isPresented: $showAlert) {
+                Alert(title: Text("Wrong Format!"),
+                      message: Text("Enter correct value from 0 to 255"))
+                }
                 .multilineTextAlignment(.center)
                 .frame(width: 45)
                 .foregroundColor(.white)
                 .overlay(RoundedRectangle(cornerRadius: 4)
-                            .stroke(lineWidth: 2)
-                            .foregroundColor(color))
+                           .stroke(lineWidth: 2)
+                           .foregroundColor(color))
         }
-      
+    }
+    
+    func checkValue(){
+        if let value = Double(sValue) {
+            if value <= 255 && value >= 0 {
+                dValue = value
+            }
+            else {
+                sValue = "\(lround(dValue))"
+                showAlert.toggle()
+            }
+        } else {
+            sValue = "\(lround(dValue))"
+            showAlert.toggle()
+        }
     }
 }
 
@@ -72,6 +100,9 @@ struct ColorView: View {
             .foregroundColor(Color(red: red/255,
                                   green: green/255,
                                   blue: blue/255))
+            .overlay(RoundedRectangle(cornerRadius: 15)
+                        .stroke(lineWidth: 2)
+                        .foregroundColor(.white))
 
             .padding()
     }
